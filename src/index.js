c15c23124cc39b41373f8465fd78b0f2e7dcc8e3
root <hyeonwoody@gmail.com>
2022-12-16 10:36:59 +0900

    init

--- src/index.js
+++ src/index.js
@@ -3,12 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import store from './store/mainStore'
+import {Provider} from 'react-redux'
+
+console.log("태초 : "+store.getState().STAGE)
+store.subscribe (()=> console.log("바뀌었다다"+store.getState().STAGE))
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    document.getElementById('root')
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
