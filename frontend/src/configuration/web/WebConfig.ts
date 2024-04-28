b0f2b059bab781aa074fc543869c559b94dfd147
hyeonwoody <hyeonwoody@gmail.com>
2024-04-28 11:57:10 +0900

    🔨CHORE : Remove '$' for formatting

--- frontend/src/configuration/web/WebConfig.ts
+++ frontend/src/configuration/web/WebConfig.ts
@@ -2,8 +2,8 @@ export class My {
     ipAddress : string;
     backEndPort : string;
     constructor() {
-        this.ipAddress = "${MY_IP_ADDRESS}";
-        this.backEndPort = "${MY_BACKEND_PORT}";
+        this.ipAddress = "{MY_IP_ADDRESS}";
+        this.backEndPort = "{MY_BACKEND_PORT}";
     }
 }
 
