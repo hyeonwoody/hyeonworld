1d12f667221436dd6512fc27e205d0ad0061044d
hyeonwoody <hyeonwoody@gmail.com>
2024-09-02 00:57:40 +0900

    ⏳RESUMPTION : Work on package structure

--- backend/src/main/java/com/toyproject/hyeonworld/HyeonworldApplication.java
+++ backend/src/main/java/com/toyproject/hyeonworld/HyeonworldApplication.java
@@ -5,10 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 
-@SpringBootApplication
-public class HyeonworldApplication {
+	@SpringBootApplication
+	public class HyeonworldApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HyeonworldApplication.class, args);
+		public static void main(String[] args) {
+			SpringApplication.run(HyeonworldApplication.class, args);
+		}
 	}
-}
