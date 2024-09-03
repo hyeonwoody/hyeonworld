4f1b35a4076189d8185c365d74e458c5bd92f6ba
hyeonwoody <hyeonwoody@gmail.com>
2024-09-03 13:17:09 +0900

    🔨CHORE : Disable previous API source codes for migration to API version 2
    - Comment out the code in `GameController`, `HomeController`, and `GameService` to disable the existing API functionality
    - Delete JdbcConfig.java to remove the previous JDBC configuration
    - Ensure a clean slate for the new implementation

--- backend/src/main/java/com/toyproject/hyeonworld/controller/GameController.java
+++ backend/src/main/java/com/toyproject/hyeonworld/controller/GameController.java
@@ -1,29 +1,29 @@
-package com.toyproject.hyeonworld.controller;
-
-import com.toyproject.hyeonworld.entity.Game;
-import com.toyproject.hyeonworld.service.GameService;
-import com.toyproject.hyeonworld.service.ThreadService;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
-
-@RestController
-@RequestMapping("/game")
-public class GameController {
-    private GameService gameService;
-
-    public GameController (GameService gameService){
-        this.gameService = gameService;
-    }
-
-    @GetMapping("/playable")
-    public ResponseEntity<List<Game>> displayGame (){
-        List<Game> gameList = gameService.displayGame();
-        return ResponseEntity.ok (gameList);
-    }
-}
+//package com.toyproject.hyeonworld.controller;
+//
+//import com.toyproject.hyeonworld.entity.Game;
+//import com.toyproject.hyeonworld.service.GameService;
+//import com.toyproject.hyeonworld.service.ThreadService;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import java.util.List;
+//import java.util.concurrent.ExecutionException;
+//import java.util.concurrent.Future;
+//
+//@RestController
+//@RequestMapping("/game")
+//public class GameController {
+//    private GameService gameService;
+//
+//    public GameController (GameService gameService){
+//        this.gameService = gameService;
+//    }
+//
+//    @GetMapping("/playable")
+//    public ResponseEntity<List<Game>> displayGame (){
+//        List<Game> gameList = gameService.displayGame();
+//        return ResponseEntity.ok (gameList);
+//    }
+//}
--- backend/src/main/java/com/toyproject/hyeonworld/service/GameService.java
+++ backend/src/main/java/com/toyproject/hyeonworld/service/GameService.java
@@ -1,24 +1,24 @@
-package com.toyproject.hyeonworld.service;
-
-import com.toyproject.hyeonworld.entity.Game;
-import com.toyproject.hyeonworld.repository.game.GameRepository;
-import com.toyproject.hyeonworld.repository.game.JdbcTemplateGameRepository;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.stream.Collectors;
-
-@Service
-public class GameService {
-
-    private final JdbcTemplateGameRepository jdbcTemplateGameRepository;
-
-
-    public GameService(JdbcTemplateGameRepository jdbcTemplateGameRepository) {
-        this.jdbcTemplateGameRepository = jdbcTemplateGameRepository;
-    }
-
-    public List<Game> displayGame() {
-        return jdbcTemplateGameRepository.findPlayableGames();
-    }
-}
+//package com.toyproject.hyeonworld.service;
+//
+//import com.toyproject.hyeonworld.entity.Game;
+//import com.toyproject.hyeonworld.repository.game.GameRepository;
+//import com.toyproject.hyeonworld.repository.game.JdbcTemplateGameRepository;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.List;
+//import java.util.stream.Collectors;
+//
+//@Service
+//public class GameService {
+//
+//    private final JdbcTemplateGameRepository jdbcTemplateGameRepository;
+//
+//
+//    public GameService(JdbcTemplateGameRepository jdbcTemplateGameRepository) {
+//        this.jdbcTemplateGameRepository = jdbcTemplateGameRepository;
+//    }
+//
+//    public List<Game> displayGame() {
+//        return jdbcTemplateGameRepository.findPlayableGames();
+//    }
+//}
