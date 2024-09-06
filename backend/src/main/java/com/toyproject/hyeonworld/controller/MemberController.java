0add1383725f574b174b36996e41b12e790d412e
hyeonwoody <hyeonwoody@gmail.com>
2024-09-06 13:29:49 +0900

    🔨CHORE : Diable previous implementation

--- backend/src/main/java/com/toyproject/hyeonworld/controller/MemberController.java
+++ backend/src/main/java/com/toyproject/hyeonworld/controller/MemberController.java
@@ -1,162 +1,162 @@
-package com.toyproject.hyeonworld.controller;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.toyproject.hyeonworld.DTO.Member.MemberAnswer;
-import com.toyproject.hyeonworld.DTO.Member.MemberCreateDTO;
-import com.toyproject.hyeonworld.DTO.Member.MemberDTO;
-import com.toyproject.hyeonworld.DTO.Member.MemberScore;
-import com.toyproject.hyeonworld.controller.sse.SseEmitters;
-
-import com.toyproject.hyeonworld.entity.Member;
-import com.toyproject.hyeonworld.service.MemberService;
-import com.toyproject.hyeonworld.service.PartyService;
-import com.toyproject.hyeonworld.service.RoundService;
-import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.Buffer;
-import java.util.HashMap;
-import java.util.List;
-import java.util.stream.Collectors;
-
-@RestController
-@RequestMapping("/member")
-public class MemberController {
-
-    private final MemberService memberService;
-
-    private final SseEmitters sseEmitters;
-
-    public MemberController(MemberService memberService, SseEmitters sseEmitters) {
-        this.memberService = memberService;
-        this.sseEmitters = sseEmitters;
-    }
-
-    @PostMapping
-    public ResponseEntity<Long> createMember (HttpServletRequest request) { //name, partyType
-        BufferedReader reader = null;
-        try {
-            reader = new BufferedReader(new InputStreamReader(request.getInputStream()));
-            String requestBody = reader.lines().collect(Collectors.joining(System.lineSeparator()));
-
-            ObjectMapper objectMapper = new ObjectMapper();
-            MemberCreateDTO memberCreateDTO = objectMapper.readValue(requestBody, MemberCreateDTO.class);
-            Long ret = memberService.create(memberCreateDTO);
-            return ResponseEntity.ok(ret);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<MemberDTO> getMemberById (@PathVariable Long id) { //name, partyType
-        MemberDTO memberDTO = memberService.findById(id);
-        return ResponseEntity.ok(memberDTO);
-    }
-
-    @PutMapping
-    public ResponseEntity<Long> editMember (HttpServletRequest request) { //name, partyType
-        BufferedReader reader = null;
-        try {
-            reader = new BufferedReader(new InputStreamReader(request.getInputStream()));
-            String requestBody = reader.lines().collect(Collectors.joining(System.lineSeparator()));
-
-            ObjectMapper objectMapper = new ObjectMapper();
-            MemberCreateDTO memberCreateDTO = objectMapper.readValue(requestBody, MemberCreateDTO.class);
-            Long ret = memberService.edit(memberCreateDTO);
-            return ResponseEntity.ok(ret);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Long> deleteMember (@PathVariable Long id){
-        memberService.delete(id);
-        return ResponseEntity.ok (id);
-    }
-
-    @PutMapping("/init")
-    public ResponseEntity<Long> init(){
-        System.out.println("INIT MEMBER");
-        Long ret = memberService.init ();
-        return ResponseEntity.ok (ret);
-    }
-
-    @PostMapping("/login-confirm")
-    public ResponseEntity<Long> loginConfirm (HttpServletRequest request){
-        System.out.println(request.getRemoteAddr());
-        String requestBody = null;
-        try {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(request.getInputStream()));
-            requestBody = reader.lines().collect(Collectors.joining(System.lineSeparator()));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-    }
-
-
-        Long loginMemberId  = memberService.login (requestBody);
-        if (loginMemberId > 0 && !sseEmitters.empty())
-            sseEmitters.send ("AddWaitingList", requestBody);
-        return ResponseEntity.ok (loginMemberId);
-    }
-
-    @PostMapping("/logout-confirm")
-    public ResponseEntity<Long> logoutConfirm (@RequestParam Long logoutId){
-        Long loginMemberId = memberService.logout (logoutId);
-        return ResponseEntity.ok (loginMemberId);
-    }
-
-    @PostMapping("/enter-game")
-    public ResponseEntity<Long> enterGame (@RequestBody HashMap<String, Long> request){
-
-        String enterMemeberName = memberService.enterGame(request.get("memberId"));
-
-        if (request.get("memberId") > 0 && enterMemeberName != null && !sseEmitters.empty())
-            sseEmitters.send ("RemoveWaitingList", enterMemeberName);
-        return ResponseEntity.ok (request.get("memberId"));
-    }
-
-    @PostMapping("/exit-game")
-    public ResponseEntity<Long> exitGame (@RequestBody HashMap<String, Long> request){
-
-        String exitMemberName = memberService.exitGame( request.get("memberId"));
-
-        if ( request.get("memberId") > 0 && exitMemberName != null && !sseEmitters.empty())
-            sseEmitters.send ("AddWaitingList", exitMemberName);
-
-        return ResponseEntity.ok (request.get("memberId"));
-    }
-
-    @GetMapping(value = "/waiting-list/init")
-    public ResponseEntity<List<String>> WaitingListInit (@RequestParam Long memberId){
-        List<String> waitngList = memberService.getWaitingList();
-        return ResponseEntity.ok (waitngList);
-    }
-
-    @PutMapping("/play/0")
-    public ResponseEntity<Long> putPlayGame0 (@RequestBody MemberAnswer memberAnswer){
-        Long playMemberId = memberService.putPlayGame0 (memberAnswer);
-
-        return ResponseEntity.ok (playMemberId);
-    }
-
-    @PutMapping("/score/0")
-    public ResponseEntity<Long> putScore0 (@RequestBody HashMap<String, Long> request){
-        System.out.println(" 스코어 "+request.get("correct"));
-        List<Member> member = memberService.putScore (request);
-        return ResponseEntity.ok (request.get("score"));
-    }
-
-    @GetMapping("/ranking")
-    public ResponseEntity<HashMap<String, List<MemberScore>>> getRanking (){
-        HashMap<String, List<MemberScore>> ret = new HashMap<>();
-        ret = memberService.getRanking ();
-
-        return ResponseEntity.ok (ret);
-    }
-}
+//package com.toyproject.hyeonworld.controller;
+//
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import com.toyproject.hyeonworld.DTO.Member.MemberAnswer;
+//import com.toyproject.hyeonworld.DTO.Member.MemberCreateDTO;
+//import com.toyproject.hyeonworld.DTO.Member.MemberDTO;
+//import com.toyproject.hyeonworld.DTO.Member.MemberScore;
+//import com.toyproject.hyeonworld.controller.sse.SseEmitters;
+//
+//import com.toyproject.hyeonworld.entity.Member;
+//import com.toyproject.hyeonworld.service.MemberService;
+//import com.toyproject.hyeonworld.service.PartyService;
+//import com.toyproject.hyeonworld.service.RoundService;
+//import jakarta.servlet.http.HttpServletRequest;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.*;
+//
+//import java.io.BufferedReader;
+//import java.io.IOException;
+//import java.io.InputStreamReader;
+//import java.nio.Buffer;
+//import java.util.HashMap;
+//import java.util.List;
+//import java.util.stream.Collectors;
+//
+//@RestController
+//@RequestMapping("/member")
+//public class MemberController {
+//
+//    private final MemberService memberService;
+//
+//    private final SseEmitters sseEmitters;
+//
+//    public MemberController(MemberService memberService, SseEmitters sseEmitters) {
+//        this.memberService = memberService;
+//        this.sseEmitters = sseEmitters;
+//    }
+//
+//    @PostMapping
+//    public ResponseEntity<Long> createMember (HttpServletRequest request) { //name, partyType
+//        BufferedReader reader = null;
+//        try {
+//            reader = new BufferedReader(new InputStreamReader(request.getInputStream()));
+//            String requestBody = reader.lines().collect(Collectors.joining(System.lineSeparator()));
+//
+//            ObjectMapper objectMapper = new ObjectMapper();
+//            MemberCreateDTO memberCreateDTO = objectMapper.readValue(requestBody, MemberCreateDTO.class);
+//            Long ret = memberService.create(memberCreateDTO);
+//            return ResponseEntity.ok(ret);
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
+//
+//    @GetMapping("/{id}")
+//    public ResponseEntity<MemberDTO> getMemberById (@PathVariable Long id) { //name, partyType
+//        MemberDTO memberDTO = memberService.findById(id);
+//        return ResponseEntity.ok(memberDTO);
+//    }
+//
+//    @PutMapping
+//    public ResponseEntity<Long> editMember (HttpServletRequest request) { //name, partyType
+//        BufferedReader reader = null;
+//        try {
+//            reader = new BufferedReader(new InputStreamReader(request.getInputStream()));
+//            String requestBody = reader.lines().collect(Collectors.joining(System.lineSeparator()));
+//
+//            ObjectMapper objectMapper = new ObjectMapper();
+//            MemberCreateDTO memberCreateDTO = objectMapper.readValue(requestBody, MemberCreateDTO.class);
+//            Long ret = memberService.edit(memberCreateDTO);
+//            return ResponseEntity.ok(ret);
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
+//
+//    @DeleteMapping("/{id}")
+//    public ResponseEntity<Long> deleteMember (@PathVariable Long id){
+//        memberService.delete(id);
+//        return ResponseEntity.ok (id);
+//    }
+//
+//    @PutMapping("/init")
+//    public ResponseEntity<Long> init(){
+//        System.out.println("INIT MEMBER");
+//        Long ret = memberService.init ();
+//        return ResponseEntity.ok (ret);
+//    }
+//
+//    @PostMapping("/login-confirm")
+//    public ResponseEntity<Long> loginConfirm (HttpServletRequest request){
+//        System.out.println(request.getRemoteAddr());
+//        String requestBody = null;
+//        try {
+//            BufferedReader reader = new BufferedReader(new InputStreamReader(request.getInputStream()));
+//            requestBody = reader.lines().collect(Collectors.joining(System.lineSeparator()));
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//    }
+//
+//
+//        Long loginMemberId  = memberService.login (requestBody);
+//        if (loginMemberId > 0 && !sseEmitters.empty())
+//            sseEmitters.send ("AddWaitingList", requestBody);
+//        return ResponseEntity.ok (loginMemberId);
+//    }
+//
+//    @PostMapping("/logout-confirm")
+//    public ResponseEntity<Long> logoutConfirm (@RequestParam Long logoutId){
+//        Long loginMemberId = memberService.logout (logoutId);
+//        return ResponseEntity.ok (loginMemberId);
+//    }
+//
+//    @PostMapping("/enter-game")
+//    public ResponseEntity<Long> enterGame (@RequestBody HashMap<String, Long> request){
+//
+//        String enterMemeberName = memberService.enterGame(request.get("memberId"));
+//
+//        if (request.get("memberId") > 0 && enterMemeberName != null && !sseEmitters.empty())
+//            sseEmitters.send ("RemoveWaitingList", enterMemeberName);
+//        return ResponseEntity.ok (request.get("memberId"));
+//    }
+//
+//    @PostMapping("/exit-game")
+//    public ResponseEntity<Long> exitGame (@RequestBody HashMap<String, Long> request){
+//
+//        String exitMemberName = memberService.exitGame( request.get("memberId"));
+//
+//        if ( request.get("memberId") > 0 && exitMemberName != null && !sseEmitters.empty())
+//            sseEmitters.send ("AddWaitingList", exitMemberName);
+//
+//        return ResponseEntity.ok (request.get("memberId"));
+//    }
+//
+//    @GetMapping(value = "/waiting-list/init")
+//    public ResponseEntity<List<String>> WaitingListInit (@RequestParam Long memberId){
+//        List<String> waitngList = memberService.getWaitingList();
+//        return ResponseEntity.ok (waitngList);
+//    }
+//
+//    @PutMapping("/play/0")
+//    public ResponseEntity<Long> putPlayGame0 (@RequestBody MemberAnswer memberAnswer){
+//        Long playMemberId = memberService.putPlayGame0 (memberAnswer);
+//
+//        return ResponseEntity.ok (playMemberId);
+//    }
+//
+//    @PutMapping("/score/0")
+//    public ResponseEntity<Long> putScore0 (@RequestBody HashMap<String, Long> request){
+//        System.out.println(" 스코어 "+request.get("correct"));
+//        List<Member> member = memberService.putScore (request);
+//        return ResponseEntity.ok (request.get("score"));
+//    }
+//
+//    @GetMapping("/ranking")
+//    public ResponseEntity<HashMap<String, List<MemberScore>>> getRanking (){
+//        HashMap<String, List<MemberScore>> ret = new HashMap<>();
+//        ret = memberService.getRanking ();
+//
+//        return ResponseEntity.ok (ret);
+//    }
+//}
--- backend/src/main/java/com/toyproject/hyeonworld/controller/PartyController.java
+++ backend/src/main/java/com/toyproject/hyeonworld/controller/PartyController.java
@@ -1,98 +1,98 @@
-package com.toyproject.hyeonworld.controller;
-
-import com.toyproject.hyeonworld.DTO.Game.CurrentGameDTO;
-import com.toyproject.hyeonworld.DTO.Member.MemberDTO;
-import com.toyproject.hyeonworld.DTO.PartyInitDTO;
-import com.toyproject.hyeonworld.DTO.Submission.SubmissionVO;
-import com.toyproject.hyeonworld.service.MemberService;
-import com.toyproject.hyeonworld.service.PartyService;
-import com.toyproject.hyeonworld.service.RoundService;
-import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-@RestController
-@RequestMapping("/party")
-public class PartyController {
-
-    private final PartyService partyService;
-    private final MemberService memberService;
-
-
-    public PartyController(PartyService partyService, MemberService memberService) {
-        this.partyService = partyService;
-        this.memberService = memberService;
-    }
-
-    @PutMapping("/current-game")
-    public ResponseEntity<Boolean> openGame (@RequestBody CurrentGameDTO gameDTO){
-        partyService.open(gameDTO.getId());
-
-        return ResponseEntity.ok (true);
-    }
-
-    @PutMapping("/target")
-    public ResponseEntity<Boolean> putTarget (@RequestBody MemberDTO memberDTO){
-
-        partyService.putTarget(memberDTO.getName());
-        return ResponseEntity.ok (true);
-    }
-
-    @PostMapping("/init")
-    public ResponseEntity<Boolean> initParty (@RequestBody PartyInitDTO partyInitDTO){
-        System.out.println("party init");
-        partyService.init(partyInitDTO);
-        memberService.init();
-        return ResponseEntity.ok (true);
-    }
-
-    @GetMapping("/current-game")
-    public ResponseEntity<Integer> getCurrentGame (){
-
-        System.out.println("send current-game");
-
-
-
-            Integer currentGame = partyService.getCurrentGameQuery();
-            System.out.println("CURRENTGAME VALUE : "+currentGame);
-            return ResponseEntity.ok(currentGame);
-
-
-
-//        System.out.println("HTTP Method: " + request.getMethod());
-//        System.out.println("Request URI: " + request.getRequestURI());
+//package com.toyproject.hyeonworld.controller;
 //
-//        // Print headers
-//        Enumeration<String> headerNames = request.getHeaderNames();
-//        while (headerNames.hasMoreElements()) {
-//            String headerName = headerNames.nextElement();
-//            String headerValue = request.getHeader(headerName);
-//            System.out.println("Header: " + headerName + " = " + headerValue);
-//        }
+//import com.toyproject.hyeonworld.DTO.Game.CurrentGameDTO;
+//import com.toyproject.hyeonworld.DTO.Member.MemberDTO;
+//import com.toyproject.hyeonworld.DTO.PartyInitDTO;
+//import com.toyproject.hyeonworld.DTO.Submission.SubmissionVO;
+//import com.toyproject.hyeonworld.service.MemberService;
+//import com.toyproject.hyeonworld.service.PartyService;
+//import com.toyproject.hyeonworld.service.RoundService;
+//import jakarta.servlet.http.HttpServletRequest;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.*;
 //
-//        // Print parameters
-//        Map<String, String[]> parameterMap = request.getParameterMap();
-//        for (Map.Entry<String, String[]> entry : parameterMap.entrySet()) {
-//            String paramName = entry.getKey();
-//            String[] paramValues = entry.getValue();
-//            System.out.println("Parameter: " + paramName + " = " + Arrays.toString(paramValues));
-//        }
+//@RestController
+//@RequestMapping("/party")
+//public class PartyController {
 //
-//        // Print other details as needed
+//    private final PartyService partyService;
+//    private final MemberService memberService;
 //
-//        // Example: Print Remote Address
-//        System.out.println("Remote Address: " + request.getRemoteAddr());
-//        System.out.println("Source Port: " + request.getRemotePort());
-
-
-    }
-    @GetMapping("/target")
-    public ResponseEntity<SubmissionVO> getTarget (){
-
-        SubmissionVO submissionVO = partyService.getTarget();
-
-        return ResponseEntity.ok (submissionVO);
-    }
-
-
-}
+//
+//    public PartyController(PartyService partyService, MemberService memberService) {
+//        this.partyService = partyService;
+//        this.memberService = memberService;
+//    }
+//
+//    @PutMapping("/current-game")
+//    public ResponseEntity<Boolean> openGame (@RequestBody CurrentGameDTO gameDTO){
+//        partyService.open(gameDTO.getId());
+//
+//        return ResponseEntity.ok (true);
+//    }
+//
+//    @PutMapping("/target")
+//    public ResponseEntity<Boolean> putTarget (@RequestBody MemberDTO memberDTO){
+//
+//        partyService.putTarget(memberDTO.getName());
+//        return ResponseEntity.ok (true);
+//    }
+//
+//    @PostMapping("/init")
+//    public ResponseEntity<Boolean> initParty (@RequestBody PartyInitDTO partyInitDTO){
+//        System.out.println("party init");
+//        partyService.init(partyInitDTO);
+//        memberService.init();
+//        return ResponseEntity.ok (true);
+//    }
+//
+//    @GetMapping("/current-game")
+//    public ResponseEntity<Integer> getCurrentGame (){
+//
+//        System.out.println("send current-game");
+//
+//
+//
+//            Integer currentGame = partyService.getCurrentGameQuery();
+//            System.out.println("CURRENTGAME VALUE : "+currentGame);
+//            return ResponseEntity.ok(currentGame);
+//
+//
+//
+////        System.out.println("HTTP Method: " + request.getMethod());
+////        System.out.println("Request URI: " + request.getRequestURI());
+////
+////        // Print headers
+////        Enumeration<String> headerNames = request.getHeaderNames();
+////        while (headerNames.hasMoreElements()) {
+////            String headerName = headerNames.nextElement();
+////            String headerValue = request.getHeader(headerName);
+////            System.out.println("Header: " + headerName + " = " + headerValue);
+////        }
+////
+////        // Print parameters
+////        Map<String, String[]> parameterMap = request.getParameterMap();
+////        for (Map.Entry<String, String[]> entry : parameterMap.entrySet()) {
+////            String paramName = entry.getKey();
+////            String[] paramValues = entry.getValue();
+////            System.out.println("Parameter: " + paramName + " = " + Arrays.toString(paramValues));
+////        }
+////
+////        // Print other details as needed
+////
+////        // Example: Print Remote Address
+////        System.out.println("Remote Address: " + request.getRemoteAddr());
+////        System.out.println("Source Port: " + request.getRemotePort());
+//
+//
+//    }
+//    @GetMapping("/target")
+//    public ResponseEntity<SubmissionVO> getTarget (){
+//
+//        SubmissionVO submissionVO = partyService.getTarget();
+//
+//        return ResponseEntity.ok (submissionVO);
+//    }
+//
+//
+//}
