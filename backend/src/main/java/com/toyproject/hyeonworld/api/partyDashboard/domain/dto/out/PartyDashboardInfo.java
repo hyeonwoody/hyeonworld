2415b6ceb20bafa86427febc1d16fbcfdf3b6c88
hyeonwoody <hyeonwoody@gmail.com>
2024-09-07 20:43:10 +0900

    🎨REFACTOR : Fix fileds in `PartyDashboardInfo` for improved accessibility and consistency

--- backend/src/main/java/com/toyproject/hyeonworld/api/partyDashboard/domain/dto/out/PartyDashboardInfo.java
+++ backend/src/main/java/com/toyproject/hyeonworld/api/partyDashboard/domain/dto/out/PartyDashboardInfo.java
@@ -2,14 +2,15 @@ package com.toyproject.hyeonworld.api.partyDashboard.domain.dto.out;
 
 import com.toyproject.hyeonworld.api.partyDashboard.infrastructure.entity.PartyDashboard;
 import com.toyproject.hyeonworld.common.mapper.ObjectrMapper;
+import lombok.Getter;
 
 /**
  * @author : hyeonwoody@gmail.com
  * @since : 24. 9. 5.
  */
-
+@Getter
 public class PartyDashboardInfo {
-  private Long partyid;
+  private Long partyId;
   private Long currentGameId;
   private Byte currentGameStage;
   public static PartyDashboardInfo from (PartyDashboard partyDashboard) {
