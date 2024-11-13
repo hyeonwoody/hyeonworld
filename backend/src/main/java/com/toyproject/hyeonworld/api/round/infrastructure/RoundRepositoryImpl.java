82882de9a64e4c525608a319d50be7744f4bb76f
hyeonwoody <hyeonwoody@gmail.com>
2024-11-14 01:29:00 +0900

    🎨REFACTOR : Update `RoundRepository` to inject `RoundJdbcTemplateRepository` interface

--- backend/src/main/java/com/toyproject/hyeonworld/api/round/infrastructure/RoundRepositoryImpl.java
+++ backend/src/main/java/com/toyproject/hyeonworld/api/round/infrastructure/RoundRepositoryImpl.java
@@ -1,7 +1,7 @@
 package com.toyproject.hyeonworld.api.round.infrastructure;
 
 import com.toyproject.hyeonworld.api.round.infrastructure.entity.Round;
-import com.toyproject.hyeonworld.api.round.infrastructure.jdbc.RoundJdbcTemplateRepositoryImpl;
+import com.toyproject.hyeonworld.api.round.infrastructure.jdbc.RoundjdbcTemplateRepository;
 import com.toyproject.hyeonworld.api.round.infrastructure.jpa.RoundJpaRepository;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
@@ -17,11 +17,11 @@ import org.springframework.stereotype.Repository;
 @RequiredArgsConstructor
 public class RoundRepositoryImpl implements RoundRepository{
   private final RoundJpaRepository roundJpaRepository;
-  private final RoundJdbcTemplateRepositoryImpl roundJdbcTemplateRepositoryImpl;
+  private final RoundjdbcTemplateRepository roundJdbcTemplateRepository;
 
   @Override
   public Round insert(Round round) {
-    return roundJdbcTemplateRepositoryImpl.insert(round);
+    return roundJdbcTemplateRepository.insert(round);
   }
 
   @Override
@@ -31,11 +31,11 @@ public class RoundRepositoryImpl implements RoundRepository{
 
   @Override
   public Round update(Round round) {
-    return roundJdbcTemplateRepositoryImpl.update(round);
+    return roundJdbcTemplateRepository.update(round);
   }
 
   @Override
   public Optional<Round> findByPartyId(long partyId) {
-    return roundJdbcTemplateRepositoryImpl.findByPartyId(partyId);
+    return roundJdbcTemplateRepository.findByPartyId(partyId);
   }
 }
